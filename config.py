4739ef9a2d325ba4b1c1d2d3daf8731f21463eca
nrecover4587-rgb <nrecover4587@gmail.com>
2026-02-21 14:36:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = os.environ.get("API_ID","9301087")
 API_HASH = os.environ.get("API_HASH","cbabdb3f23de6326352ef3ac26338d9c")
 BOT_TOKEN = os.environ.get("BOT_TOKEN","7879064100:AAFRr4wHNLZaN-LN4E5JYeTi-Qtgc0q1HSc")
 DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://bob:bobfiles1@bob.sp1vv.mongodb.net/?retryWrites=true&w=majority&appName=bob")
-BOT_USERNAME = os.environ.get("BOT_USERNAME","autoo_deletee_bot") # Without @
+BOT_USERNAME = os.environ.get("BOT_USERNAME","Cleartextsbot") # Without @
 FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "codexempire")  # without @
 PORT = int(os.environ.get("PORT", 8080))  # for Flask binding (Koyeb)
 OWNER_ID = int(os.environ.get("OWNER_ID", "7771222218"))  # your Telegram user ID
